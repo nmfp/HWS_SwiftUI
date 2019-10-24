629e8a3bf376226b5728ab3e26a0483df09c7c72
Nuno Pereira <>
2019-10-24 09:24:47 +0100

    Initial project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Nuno Pereira on 24/10/2019.
+//  Copyright © 2019 Nuno Pereira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
