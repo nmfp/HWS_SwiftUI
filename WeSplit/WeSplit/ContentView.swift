598585afdf86735634e9108571838576fa535f4f
Nuno Pereira <>
2019-10-25 08:43:43 +0100

    Challenge changing picker to textfield, new section with header

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 2
     
     private let tipValues = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(Int(numberOfPeople) ?? 1)
+        return totalAmount / peopleCount
+    }
+    
+    var totalAmount: Double {
+        let tipAmount = Double(tipValues[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipAmount
+        let grandTotal = orderAmount + tipValue
+        return grandTotal
+    }
     
     var body: some View {
         NavigationView {
@@ -22,23 +36,24 @@ struct ContentView: View {
                     TextField("Amount: ", text: $checkAmount)
                         .keyboardType(.decimalPad)
                     
-                    Picker("Amount of people", selection: $numberOfPeople) {
-                        ForEach (2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                    }
+                    TextField("Amount of people:", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
                 }
                 
                 Section(header: Text("How much tip you want to leave?")) {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach (0 ..< tipValues.count) {
-                            Text("\(self.tipValues[$0])%")
+                        ForEach (0 ..< tipValues.count) { value in
+                            Text("\(self.tipValues[value])%")
                         }
                     }.pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section {
-                    Text("$\(checkAmount)")
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Total Amount")) {
+                    Text("$\(totalAmount, specifier: "%.2f")")
                 }
             }.navigationBarTitle("WeSplit")
         }
