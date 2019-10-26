69c339ff06ff105fff71d2c87feace142b433fa1
Nuno Pereira <>
2019-10-26 18:53:01 +0100

    Challenge day 19 Unit Converter initial commit

---
+++ Challenge_day_19_Unit_Converter/Challenge_day_19_Unit_Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge_day_19_Unit_Converter
+//
+//  Created by Nuno Pereira on 26/10/2019.
+//  Copyright © 2019 Nuno Pereira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
