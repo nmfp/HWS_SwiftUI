4f7f561e49f1773a202dba99ffb163eb6d40083f
Nuno Pereira <>
2019-11-02 23:55:18 +0000

    Final commit

--- Challenge_day_19_Unit_Converter/Challenge_day_19_Unit_Converter/ContentView.swift
+++ Challenge_day_19_Unit_Converter/Challenge_day_19_Unit_Converter/ContentView.swift
@@ -8,9 +8,71 @@
 
 import SwiftUI
 
+enum Units: String, CaseIterable {
+    case meters = "m", kilometers = "km", feet = "ft", yards = "yds", miles = "mil"
+    
+    var toMeters: Double {
+        switch self {
+        case .meters:
+            return 1
+        case .kilometers:
+            return 1000
+        case .feet:
+            return 0.33
+        case .yards:
+            return 1.6
+        case .miles:
+            return 1600
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var inputUnit = 2
+    @State private var outputUnit = 2
+    @State private var inputValue = ""
+    
+    
+    var totalConverted: Double {
+        guard inputUnit != outputUnit else { return Double(inputValue) ?? 0.0 }
+        let value = Double(inputValue) ?? 0.0
+        let inputInMeters = value * Units.allCases[inputUnit].toMeters
+        let outputValue = inputInMeters / Units.allCases[outputUnit].toMeters
+        return outputValue
+    }
+    
+    var result: CGFloat = 0
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section(header: Text("Choose your unit to convert")) {
+                    TextField("Your unit", text: $inputValue)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("Choose your input unit")) {
+                    Picker("Input unit", selection: $inputUnit) {
+                        ForEach(0..<Units.allCases.count) {
+                            Text(Units.allCases[$0].rawValue)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Choose your output unit")) {
+                    Picker("Output unit", selection: $outputUnit) {
+                        ForEach(0..<Units.allCases.count) {
+                            Text(Units.allCases[$0].rawValue)
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Total:")) {
+                    Text("\(totalConverted, specifier: "%.3f")")
+                }
+            }.navigationBarTitle("Unit Converter")
+        }
     }
 }
 
